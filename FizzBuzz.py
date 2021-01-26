93f1055cab9e3bc34f93e3426c78bf6503ffadaf
Mario Lastra <nxtmario@hotmail.com>
2021-01-26 10:37:27 -0500

    Changed argument name

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,12 +3,12 @@
 
 fizzBuzzList = []
 
-def FizzBuzz(input):
+def FizzBuzz(number):
     #   Create a list that stores input, fizz, buzz, and fizzbuzz. 
-    fb_list = [input, "Fizz", "Buzz", "FizzBuzz"]
+    fb_list = [number, "Fizz", "Buzz", "FizzBuzz"]
 
-    fizz = (input % 3 == 0)     #   If fizz is true, fizz = 1
-    buzz = (input % 5 == 0) * 2 #   If buzz is true, buzz = 2
+    fizz = (number % 3 == 0)     #   If fizz is true, fizz = 1
+    buzz = (number % 5 == 0) * 2 #   If buzz is true, buzz = 2
 
     #   Get a value from the list at index fizz+buzz. Ex: fizz+buzz = 0 Return input | fizz+buzz = 3 Return "FizzBuzz"
     return fb_list[fizz+buzz]
